98c0005cf81a96928b822712efe172a58ec1cb43
thucphamtubon <admin@thucphamtubon.com>
2023-12-17 12:09:39 +0700

    update

---
+++ _next/static/webpack/styles.176623e424e8be971490.hot-update.js
@@ -0,0 +1,81 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./components/Introduce/Introduce.scss":
+false,
+
+/***/ "./components/ItemServices/ItemService/ItemService.scss":
+false,
+
+/***/ "./components/ItemServices/ItemServices.scss":
+false,
+
+/***/ "./components/News/News.scss":
+false,
+
+/***/ "./components/News/NewsItem/NewsItem.scss":
+false,
+
+/***/ "./components/Partners/Partners.scss":
+false,
+
+/***/ "./components/Reason/Reason.scss":
+false,
+
+/***/ "./components/Reason/ReasonItem/ReasonItem.scss":
+false,
+
+/***/ "./components/Slider/SliderHeader.scss":
+false,
+
+/***/ "./components/Story/Story.scss":
+false,
+
+/***/ "./components/shared/Button/Button.scss":
+false,
+
+/***/ "./components/shared/Title/Title.scss":
+false,
+
+/***/ "./styles/dich-vu.scss":
+false,
+
+/***/ 10:
+false,
+
+/***/ 11:
+false,
+
+/***/ 12:
+false,
+
+/***/ 13:
+false,
+
+/***/ 14:
+false,
+
+/***/ 15:
+false,
+
+/***/ 16:
+false,
+
+/***/ 17:
+false,
+
+/***/ 19:
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false,
+
+/***/ 8:
+false,
+
+/***/ 9:
+false
+
+})
